1ce794777a07fd95b10873bd5e3fa5c5be97e45b
Diego Santos <dsaantos1995@gmail.com>
2017-01-07 20:05:10 -0200

    novas funcionalidades

--- javascript/singlepage.js
+++ javascript/singlepage.js
@@ -1,27 +1,45 @@
 function newvalue(){
 	let novoli = document.getElementById('novoli').value;
-		if(novoli === ''){
-			alert("preencha algo para incluir.");
-		}
+	if(novoli === ''){
+		alert("preencha algo para incluir.");
+		return false;
+	}
 
 	let namesList = document.getElementById('names');
 	let list = document.createElement('li');
-	console.log(list.innerHTML = novoli);
+	list.innerHTML = novoli;
 	namesList.appendChild(list);
 
+	limparNovoli();
+
+	$('#names > li').on('click', function(e){
+		this.remove();
+	});	
 };
 
 function ordenar(){
-	let arr = [];
+	var arr = [];
 	let namesList = document.getElementById("names");
 	let list = namesList.getElementsByTagName("li");
 	for (var i = 0; i < list.length; i++) {
-	    let z = list[i].innerText;
-	    arr.push(z);
-	    arr.sort();
+		let z = list[i].innerText;
+		arr.push(z);
+		arr.sort();
 	}
 	$('li').remove();
 	arr.forEach(function(index, val) {
 		namesList.innerHTML = namesList.innerHTML + '<li>' + arr[val] + '</li>';
 	});
+
+	$('#names > li').on('click', function(e){
+		this.remove();
+	});
+}
+
+$('#names > li').on('click', function(e){
+	this.remove();
+});
+
+function limparNovoli(){
+	document.getElementById('novoli').value = "";
 }
