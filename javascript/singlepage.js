ee14b5f71c63604a75153e003a83a6ac5ba6303c
Diego Santos <dsaantos1995@gmail.com>
2018-04-13 11:20:13 -0300

    formatt document js

--- javascript/singlepage.js
+++ javascript/singlepage.js
@@ -1,68 +1,65 @@
-(function(){
-	'use strict';
+(function() {
+  "use strict";
 
-	window.addEventListener('DOMContentLoaded', function(){
-		var $formSubmit = document.forms['form-submit'];
-		var $ulNames = document.querySelector('ul#names');
-		var $sortNames = document.querySelector('button#sort');
-		var $nameDefault = document.querySelector('.name-default');
-		var $lisNames;
-		var li;
-		var text;
+  window.addEventListener("DOMContentLoaded", function() {
+    var $formSubmit = document.forms["form-submit"];
+    var $ulNames = document.querySelector("ul#names");
+    var $sortNames = document.querySelector("button#sort");
+    var $nameDefault = document.querySelector(".name-default");
+    var $lisNames;
+    var li;
+    var text;
 
-		$formSubmit.addEventListener('submit', formSubmited);
-		$sortNames.addEventListener('click', sortingNames);
+    $formSubmit.addEventListener("submit", formSubmited);
+    $sortNames.addEventListener("click", sortingNames);
 
-		function nameDefault(){
-				$nameDefault.style.display = 'none';
-		}
+    function nameDefault() {
+      $nameDefault.style.display = "none";
+    }
 
-		function formSubmited(e){
-			e.preventDefault();
-			
-			var input = $formSubmit.querySelector('input[type="text"]').value;
+    function formSubmited(e) {
+      e.preventDefault();
 
-			if(!!input){
-				nameDefault();
-			} else{
-				alert('Name is empty');
-				return;
-			}
+      var input = $formSubmit.querySelector('input[type="text"]').value;
 
-			createLi(input);
+      if (!!input) {
+        nameDefault();
+      } else {
+        alert("Name is empty");
+        return;
+      }
 
-			$formSubmit.reset();
-		}
+      createLi(input);
 
-		function createLi(name){
-			li = document.createElement('li');
-			text = document.createTextNode(name);
+      $formSubmit.reset();
+    }
 
-			li.appendChild(text);
-			$ulNames.appendChild(li);
-		}
+    function createLi(name) {
+      li = document.createElement("li");
+      text = document.createTextNode(name);
 
-		function sortingNames(e){
-			e.preventDefault();
-			var sortNames = [];
-			$lisNames = $ulNames.querySelectorAll('li');
+      li.appendChild(text);
+      $ulNames.appendChild(li);
+    }
 
-			for(var x = 0; x < $lisNames.length; x++){
-				var name = $lisNames[x].textContent.toLowerCase();
-				sortNames.push(name);
-				sortNames.sort();
-			}
+    function sortingNames(e) {
+      e.preventDefault();
+      var sortNames = [];
+      $lisNames = $ulNames.querySelectorAll("li");
 
-			for(var x = 0; x < $lisNames.length; x++){
-				$lisNames[x].parentNode.removeChild($lisNames[x]);
-			}
+      for (var x = 0; x < $lisNames.length; x++) {
+        var name = $lisNames[x].textContent.toLowerCase();
+        sortNames.push(name);
+        sortNames.sort();
+      }
 
-			for(var x = 0; x < sortNames.length; x++){
-				createLi(sortNames[x]);
-			}
-		}
+      for (var x = 0; x < $lisNames.length; x++) {
+        $lisNames[x].parentNode.removeChild($lisNames[x]);
+      }
 
-	});
-
-
+      for (var x = 0; x < sortNames.length; x++) {
+        createLi(sortNames[x]);
+      }
+    }
+  });
 })();
